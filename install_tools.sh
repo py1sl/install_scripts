477f4f511071a85d876aaec4d6c7c55fc575221c
Steve Lilley <py1sl@users.noreply.github.com>
2016-08-24 09:03:48 +0100

    install my own tools
    to save time installing my tools

---
+++ install_tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script builds some of the other the other tools I use with pyne.
+#
+# Run this script from any directory by issuing the command:
+# $ ./install_tools.sh
+# After the build finishes run:
+#  $ source ~/.bashrc
+# first alara
+sudo apt-get install build-essentail libtool automake
+cd $HOME
+mkdir alara
+cd alara
+git clone https://github.com/svalinn/alara.git
+cd alara
+autoreconf -fi
+mkdir bld
+cd bld
+../configure --prefix = $HOME/alara/alara/bld
+make
+make install
+#
+#
+# now other tools
+cd $HOME
+mkdir toolkit
+cd toolkit
+git clone https://github.com/py1sl/neutron_tools
+git clone https://github.com/py1sl/pyne_based_tools 
